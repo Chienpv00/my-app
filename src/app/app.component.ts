91e7386efe54a7fe796011d1dc90e3dafffac885
18520529 <18520529@gm.uit.edu.vn>
2024-07-12 04:06:39 +0000

    Test signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'myapp';
+  x = signal(0);
+  y = signal(1);
+  z = computed(() => this.x() + this.y());
 }
